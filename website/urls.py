56a9b58f76afaf4b2bdf71c3f8f4ce26894795f6
rarity96 <struradek196@gmail.com>
2023-01-17 20:36:49 +0100

    add base.html and some views

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
-
+from website import views as ex_views
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('/contact', ex_views.Contact.as_view(), name='contact'),
+    path('/blog', ex_views.Blog.as_view(), name='blog'),
+    path('/blog_single', ex_views.BlogSingle.as_view(), name='blog-single'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html', {})
+
+
+class Contact(View):
+    template_name = 'contact.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class Blog(View):
+    template_name = 'blog.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class BlogSingle(View):
+    template_name = 'blog-single.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
